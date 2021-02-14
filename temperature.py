cba704156526eb1c71c6ba0ac5c52875f9d98548
realseazonli <79042703+realseazonli@users.noreply.github.com>
2021-02-14 21:50:11 +0800

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -2,4 +2,4 @@
 temp_c = float(input('请输入摄氏温度：'))
 temp_h = temp_c*9/5+32
 if temp_c :
-    print('您输入的摄氏温度是：', temp_c, '°C，换算成华氏温度是：', temp_h, '°C')
+    print('您输入的摄氏温度是：', temp_c, '°C，换算成华氏温度是：', temp_h, '°F')
